25788d4c3d64214501891a03febb1ff6a26cf27e
Freedom Khanyile <freedom.khanyile1@gmail.com>
2021-08-03 22:33:12 +0200

    Set up main with the StreamProvider AuthModel return type.

--- src/v2/my_book_club/lib/main.dart
+++ src/v2/my_book_club/lib/main.dart
@@ -2,7 +2,9 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_book_club/models/authModel.dart';
 import 'package:my_book_club/screens/root/root.dart';
+import 'package:my_book_club/services/auth.dart';
 import 'package:my_book_club/utils/ourTheme.dart'; 
 import 'package:provider/provider.dart';
 // Import the firebase_core plugin
@@ -15,8 +17,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider(
-      create: null,
+    return StreamProvider<AuthModel>.value(
+      value: AuthService().user,
+      initialData: null,
       child: MaterialApp(
         theme: OurTheme().buildTheme(),
         debugShowCheckedModeBanner: false,
