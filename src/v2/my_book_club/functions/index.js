e87ceb2793ea97b647a7487568946772ff275277
Freedom Khanyile <freedom.khanyile1@gmail.com>
2021-08-09 10:42:12 +0200

    deployed firebase function checkForBookTransition successfully.

--- src/v2/my_book_club/functions/index.js
+++ src/v2/my_book_club/functions/index.js
@@ -1,22 +1,33 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 admin.initializeApp();
 
 const database = admin.firestore();
  
-exports.checkForBookTransition = functions.pubsub.schedule('every 1 minute').onRun(async (context) => {
+exports.checkForBookTransition = functions.pubsub.schedule('0 * * * *').onRun(async (context) => {
 
-    // write our function logic here.
-
-    const query = await database.collection("groups").where("currentBookDue", '<=', admin.firestore.Timestamp.now())
+    const query = await database.collection("groups")
+    .where("currentBookDue",'<=', admin.firestore.Timestamp.now())
     .get();
 
-    query.forEach(async group => {
+    query.forEach(async eachGroup => {     
         
-        await database.doc('groups/'+ group.id).update({
-            "currentBookDue": group.data()["nextBookDue"],
-            "currentBookId": group.data()["nextBookId"],
-            "nextBookId": "waiting"
-        });
+        // find the next index
+        var currentIndex = eachGroup.data()["indexPickingBook"];
+        var totalMembers = eachGroup.data()["members"].length;
+        var nextIndex;
+        
+        if (currentIndex >= (totalMembers - 1)) {
+            nextIndex = 0;
+        } else {
+            nextIndex = currentIndex + 1;
+        }
+
+         await database.doc('groups/'+ eachGroup.id).update({
+            "currentBookDue": eachGroup.data()["nextBookDue"],
+            "currentBookId": eachGroup.data()["nextBookId"],
+            "nextBookId": "waiting",
+            "indexPickingBook": nextIndex,
+        })
     })
 })
